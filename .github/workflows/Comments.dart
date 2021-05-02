b8550a19db0a9e42a4bda8b6a5a291d462af02d3
FelixBliss <43016973+FelixBliss@users.noreply.github.com>
2021-05-02 15:51:59 +0000

    Create Comments.dart

---
+++ .github/workflows/Comments.dart
@@ -0,0 +1,13 @@
+void main() {
+ //single line comment
+  
+  /* I will never give up on coding until 
+   * i have achieve my goal
+   * try and code because it is a brain
+   * exercise*/
+  
+  ///documentation comment is key
+  ///document you code with comments
+  
+  print("there are three types of comment. These are single line, multiline and documentation");
+}
